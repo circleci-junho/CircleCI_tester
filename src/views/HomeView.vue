6825a7ab73c01f74830131e32d821c8adba81d15
Jun <pajums0235@gmail.com>
2022-03-13 11:26:08 +0900

    Re-inital vue3.0 with ts

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <FizzBuzz />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import FizzBuzz from '@/components/FizzBuzz.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    FizzBuzz,
+  },
+});
+</script>
