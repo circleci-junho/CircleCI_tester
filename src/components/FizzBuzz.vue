ee19f383196eb5be39c9bd01353fd30f15d1b711
Jun <pajums0235@gmail.com>
2022-03-13 21:06:57 +0900

    Update scss with commonalization

--- src/components/FizzBuzz.vue
+++ src/components/FizzBuzz.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="fizzBuzz">
+    <div class="fizzBuzz__head">
+      <span>Is this FizzBuzz?</span>
+    </div>
     <div class="fizzBuzz__text">
       <span v-if="result.fizzBuzz == ``">{{result.count}}</span>
-      <span v-else> Count {{result.count}} is {{result.fizzBuzz}}!</span>
+      <span v-else>{{result.count}} is {{result.fizzBuzz}}!</span>
     </div>
     <div
       class="fizzBuzz__button"
       @click="getFizzBuzz()"
     >
-      <span>Count UP!</span>
+      <span>Count up</span>
     </div>
   </div>
 </template>
@@ -29,24 +32,35 @@ export default defineComponent({
 <style lang="scss" scoped>
 .fizzBuzz{
   align-items: center;
+  width: 35%;
+  margin: auto;
 
-  &__text{
+  %__default {
     display: flex;
     justify-content: center;
+    margin: 10px;
+  }
+
+  &__head{
+    @extend %__default;
+    font-size: 1.5em;
+  }
+
+  &__text{
+    @extend %__default;
+    font-size: 2em;
   }
 
   &__button{
-    display: flex;
-    justify-content: center;
+    @extend %__default;
+    font-size: 1em;
 
     border: 1px #757575 solid;
     border-radius: 3px;
-    padding: 3px 10px;
-    margin: 10px;
+    padding: 10px;
     cursor: pointer;
     box-shadow: 0px 3px 4px #757575;
 
-    
     &:hover{
       transform: scale(1.02);
       transition: transform .2s;
